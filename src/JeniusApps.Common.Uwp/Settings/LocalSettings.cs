83f84519a2fb26ede777f18b68f72dbe4fc65899
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-09-29 22:13:41 -0700

    Added local settings

---
+++ src/JeniusApps.Common.Uwp/Settings/LocalSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage;
+
+namespace JeniusApps.Common.Settings.Uwp
+{
+    public class LocalSettings : IUserSettings
+    {
+        private readonly IDictionary<string, object> _defaults;
+
+        /// <inheritdoc/>
+        public event EventHandler<string> SettingSet;
+
+        public LocalSettings(IDictionary<string, object> defaults)
+        {
+            _defaults = defaults ?? new Dictionary<string, object>();
+        }
+
+        /// <inheritdoc/>
+        public T Get<T>(string settingKey)
+        {
+            object result = ApplicationData.Current.LocalSettings.Values[settingKey];
+            return result is null ? GetDefault<T>(settingKey) : (T)result;
+        }
+
+        /// <inheritdoc/>
+        public void Set<T>(string settingKey, T value)
+        {
+            ApplicationData.Current.LocalSettings.Values[settingKey] = value;
+            SettingSet?.Invoke(this, settingKey);
+        }
+
+        /// <inheritdoc/>
+        public T Get<T>(string settingKey, T defaultOverride)
+        {
+            object result = ApplicationData.Current.LocalSettings.Values[settingKey];
+            return result is null ? defaultOverride : (T)result;
+        }
+
+        private T GetDefault<T>(string settingKey)
+        {
+            return _defaults.ContainsKey(settingKey)
+                ? (T)_defaults[settingKey]
+                : default;
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Settings/IUserSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace JeniusApps.Common.Settings
+{
+    /// <summary>
+    /// Interface for storing
+    /// and retrieving user settings.
+    /// </summary>
+    public interface IUserSettings
+    {
+        /// <summary>
+        /// Raised when a settings is set.
+        /// String is the key of the setting.
+        /// </summary>
+        event EventHandler<string> SettingSet;
+
+        /// <summary>
+        /// Saves settings into persistent local
+        /// storage.
+        /// </summary>
+        /// <typeparam name="T">Type of the value.</typeparam>
+        /// <param name="settingKey">The settings key to use.</param>
+        /// <param name="value">The value to save.</param>
+        void Set<T>(string settingKey, T value);
+
+        /// <summary>
+        /// Retrieves the value for the desired settings key.
+        /// </summary>
+        /// <typeparam name="T">Type of the value.</typeparam>
+        /// <param name="settingKey">The settings key to use.</param>
+        /// <returns>The desired value or returns the default value.</returns>
+        T Get<T>(string settingKey);
+
+        /// <summary>
+        /// Retrieves the value for the desired settings key.
+        /// </summary>
+        /// <typeparam name="T">Type of the value.</typeparam>
+        /// <param name="settingKey">The settings key to use.</param>
+        /// <param name="defaultOverride">The default override to use if the setting has no value.</param>
+        /// <returns>The desired value or returns the default override.</returns>
+        T Get<T>(string settingKey, T defaultOverride);
+    }
+}
