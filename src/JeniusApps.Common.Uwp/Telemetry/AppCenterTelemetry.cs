a27d46c981a76c734f1cbd8a514e1c6d592360f1
Daniel Paulino <dpaulino@users.noreply.github.com>
2021-09-29 21:52:02 -0700

    Initial commit

---
+++ src/JeniusApps.Common.Uwp/Telemetry/AppCenterTelemetry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using Windows.Globalization;
+
+namespace JeniusApps.Common.Telemetry.Uwp
+{
+    public class AppCenterTelemetry : ITelemetry
+    {
+        public AppCenterTelemetry(string apiKey)
+        {
+            AppCenter.SetCountryCode(new GeographicRegion().CodeTwoLetter);
+            AppCenter.Start(apiKey, typeof(Analytics), typeof(Crashes));
+        }
+
+        public void TrackError(Exception e, IDictionary<string, string> properties = null)
+        {
+            Crashes.TrackError(e, properties);
+        }
+
+        public void TrackEvent(string eventName, IDictionary<string, string> properties = null)
+        {
+            Analytics.TrackEvent(eventName, properties);
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Telemetry/ITelemetry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JeniusApps.Common.Telemetry
+{
+    public interface ITelemetry
+    {
+        /// <summary>
+        /// Tracks exceptions.
+        /// </summary>
+        void TrackError(Exception e, IDictionary<string, string>? properties = null);
+
+        /// <summary>
+        /// Tracks the given event and its properties.
+        /// </summary>
+        /// <param name="eventName">Name of event.</param>
+        /// <param name="properties">Optoinal properties associated with the event.</param>
+        void TrackEvent(string eventName, IDictionary<string, string>? properties = null);
+    }
+}
