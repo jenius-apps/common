b5e632b9016de862bbfde18e0fc3606b32137304
Daniel <dpaulino@users.noreply.github.com>
2022-01-23 19:07:07 -0800

    Adding localizer

---
+++ src/JeniusApps.Common.Uwp/Tools/ReswLocalizer.cs
@@ -0,0 +1,31 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace JeniusApps.Common.Tools.Uwp
+{
+    public class ReswLocalizer : ILocalizer
+    {
+        private readonly ResourceLoader _resourceLoader;
+
+        public ReswLocalizer()
+        {
+            _resourceLoader = ResourceLoader.GetForCurrentView();
+        }
+
+        /// <inheritdoc/>
+        public string GetString(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return string.Empty;
+            }
+
+            return _resourceLoader.GetString(key);
+        }
+
+        /// <inheritdoc/>
+        public string GetString(string key, string formatParam)
+        {
+            return string.Format(_resourceLoader.GetString(key), formatParam);
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Tools/ILocalizer.cs
@@ -0,0 +1,22 @@
+﻿namespace JeniusApps.Common.Tools
+{
+    /// <summary>
+    /// Interfaces for retrieving localized strings.
+    /// </summary>
+    public interface ILocalizer
+    {
+        /// <summary>
+        /// Retrieves the localized string for the given key.
+        /// </summary>
+        /// <param name="key">The resource key for the string.</param>
+        string GetString(string key);
+
+        /// <summary>
+        /// Retrieves the localized string for the given key and uses
+        /// the given parameter to format the localized string.
+        /// </summary>
+        /// <param name="key">The resource key for the string.</param>
+        /// <param name="formatParam">The parameter to use to format the localized string.</param>
+        string GetString(string key, string formatParam);
+    }
+}
