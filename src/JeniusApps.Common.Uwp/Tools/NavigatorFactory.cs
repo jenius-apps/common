f1462e78fc47cdc2b057ca91959636ccf3785ea0
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-05-06 01:28:06 -0700

    Added navigator factory

---
+++ src/JeniusApps.Common.Uwp/Tools/NavigatorFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace JeniusApps.Common.Tools.Uwp
+{
+    public class NavigatorFactory : INavigatorFactory
+    {
+        private readonly Dictionary<string, INavigator> _navigators = new();
+
+        /// <inheritdoc/>
+        public INavigator Create(
+            string navigatorName, 
+            object? constructorParameter, 
+            object? frame)
+        {
+            if (_navigators.TryGetValue(navigatorName, out INavigator navigator))
+            {
+                return navigator;
+            }
+
+            if (constructorParameter is IReadOnlyDictionary<string, Type> dictionary &&
+                frame is Frame f)
+            {
+                var newNavigator = new Navigator(dictionary);
+                newNavigator.InitializeFrame(f);
+                _navigators.TryAdd(navigatorName, newNavigator);
+                return newNavigator;
+            }
+            else
+            {
+                throw new ArgumentException("The constructor parameter or the frame object provided are invalid.");
+            }
+        }
+
+        /// <inheritdoc/>
+        public INavigator? Get(string navigatorName)
+        {
+            if (_navigators.TryGetValue(navigatorName, out INavigator navigator))
+            {
+                return navigator;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Tools/INavigatorFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace JeniusApps.Common.Tools
+{
+    /// <summary>
+    /// Interface for creating INavigator instances.
+    /// </summary>
+    public interface INavigatorFactory
+    {
+        /// <summary>
+        /// Creates a new INavigator instance using the given parameters.
+        /// </summary>
+        /// <param name="navigatorName">Key name for the navigator. Can be used to retrieve the existing instance after it's been created.</param>
+        /// <param name="constructorParameter">The parameter to be supplied to the constructor of the INavigator instance.</param>
+        /// <param name="frame">The frame object to be associated with the INavigator instance.</param>
+        /// <returns>Returns the newly created INavigator instance.</returns>
+        INavigator Create(string navigatorName, object? constructorParameter, object? frame);
+
+        /// <summary>
+        /// Retrieves an INavigator instance with the given navigator key name.
+        /// </summary>
+        /// <param name="navigatorName">The key name of the INavigator to fetch.</param>
+        /// <returns>Returns the requested INavigator instance or null if it doesn't exist.</returns>
+        INavigator? Get(string navigatorName);
+    }
+}
