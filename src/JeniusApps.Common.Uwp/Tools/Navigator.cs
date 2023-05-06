f8859cba334014887e390c2fa399960456be04b6
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-05-06 00:37:19 -0700

    Added navigator

---
+++ src/JeniusApps.Common.Uwp/Tools/Navigator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+#nullable enable
+
+namespace JeniusApps.Common.Tools.Uwp
+{
+    public class Navigator : INavigator
+    {
+        private readonly IReadOnlyDictionary<string, Type> _pageTypeMap;
+        private object? _frame;
+
+        /// <inheritdoc/>
+        public event EventHandler<string>? PageNavigated;
+
+        public Navigator(IReadOnlyDictionary<string, Type> pageTypeMap)
+        {
+            _pageTypeMap = pageTypeMap;
+        }
+
+        /// <inheritdoc/>
+        public void InitializeFrame(object frame) => _frame ??= frame;
+
+        /// <inheritdoc/>
+        public void NavigateTo(string pageKey, object? navArgs = null)
+        {
+            if (!_pageTypeMap.TryGetValue(pageKey, out Type pageType))
+            {
+                return;
+            }
+
+            if (_frame is Frame f)
+            {
+                f.Navigate(pageType, navArgs, new SuppressNavigationTransitionInfo());
+                PageNavigated?.Invoke(this, pageKey);
+            }
+        }
+
+        /// <inheritdoc/>
+        public void GoBack(PageTransition transition = PageTransition.None)
+        {
+            if (_frame is Frame f && f.CanGoBack)
+            {
+                f.GoBack(ToTransitionInfo(transition));
+            }
+        }
+
+        private NavigationTransitionInfo ToTransitionInfo(PageTransition transition)
+        {
+            return transition switch
+            {
+                PageTransition.None => new SuppressNavigationTransitionInfo(),
+                PageTransition.Drill => new DrillInNavigationTransitionInfo(),
+                PageTransition.Slide => new SlideNavigationTransitionInfo(),
+                _ => new SuppressNavigationTransitionInfo()
+            };
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Tools/INavigator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace JeniusApps.Common.Tools
+{
+    /// <summary>
+    /// Interface that aids with page navigation.
+    /// </summary>
+    public interface INavigator
+    {
+        /// <summary>
+        /// Raised when a navigation was performed successfully.
+        /// The string payload is the pageKey.
+        /// </summary>
+        event EventHandler<string>? PageNavigated;
+
+        /// <summary>
+        /// Initializes the frame that will be used for navigation.
+        /// </summary>
+        /// <param name="frame">The frame to navigate.</param>
+        void InitializeFrame(object frame);
+
+        /// <summary>
+        /// Navigates the frame to the given page.
+        /// </summary>
+        /// <param name="pageKey">The page to naviate to.</param>
+        /// <param name="navArgs">Optional. An object that will be passed to the destination page.</param>
+        void NavigateTo(string pageKey, object? navArgs = null);
+
+        /// <summary>
+        /// Safely goes back one level in the frame's navigation stack.
+        /// </summary>
+        /// <param name="transition">Optional. The transition to use when navigating back.</param>
+        void GoBack(PageTransition transition = PageTransition.None);
+    }
+
+    /// <summary>
+    /// Enums that represent supported page transitions.
+    /// </summary>
+    public enum PageTransition
+    {
+        /// <summary>
+        /// Represents no page transition animation.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Represents a drill in or out transition animation.
+        /// </summary>
+        Drill,
+
+        /// <summary>
+        /// Represents a slide left or right transition animation.
+        /// </summary>
+        Slide
+    }
+}
