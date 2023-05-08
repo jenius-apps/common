1bdf9405e3898b785de764b218c838fee0a686af
Daniel <dpaulino@users.noreply.github.com>
2023-05-08 08:35:42 -0700

    Added uri launcher

---
+++ src/JeniusApps.Common/Tools/IUriLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JeniusApps.Common.Tools
+{
+    /// <summary>
+    /// Interface for launching URIs.
+    /// </summary>
+    public interface IUriLauncher
+    {
+        /// <summary>
+        /// Launches the give URI.
+        /// </summary>
+        /// <param name="uri">The URI to launch.</param>
+        /// <returns>True if launch was successful. False, otherwise.</returns>
+        Task<bool> LaunchUriAsync(Uri uri);
+    }
+}
