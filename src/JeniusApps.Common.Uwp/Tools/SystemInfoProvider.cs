622018fef8c146b63735721abb3d303a3d1459b5
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-05-09 08:35:14 -0700

    Added system info provider

---
+++ src/JeniusApps.Common.Uwp/Tools/SystemInfoProvider.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Toolkit.Uwp.Helpers;
+using System;
+using Windows.Storage;
+using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+
+#nullable enable
+
+namespace JeniusApps.Common.Tools.Uwp
+{
+    public class SystemInfoProvider : ISystemInfoProvider
+    {
+        private bool? _isWin11;
+
+        /// <inheritdoc/>
+        public DateTime FirstUseDate()
+        {
+            return SystemInformation.Instance.FirstUseTime;
+        }
+
+        /// <inheritdoc/>
+        public string GetCulture()
+        {
+            return SystemInformation.Instance.Culture.Name;
+        }
+
+        /// <inheritdoc/>
+        public bool IsCompact()
+        {
+            return ApplicationView.GetForCurrentView().ViewMode == ApplicationViewMode.CompactOverlay;
+        }
+
+        /// <inheritdoc/>
+        public string GetDeviceFamily()
+        {
+            return AnalyticsInfo.VersionInfo.DeviceFamily;
+        }
+
+        /// <inheritdoc/>
+        public bool IsFirstRun()
+        {
+            return SystemInformation.Instance.IsFirstRun;
+        }
+
+        /// <inheritdoc/>
+        public bool IsWin11()
+        {
+            _isWin11 ??= Windows.Foundation.Metadata.ApiInformation.IsApiContractPresent(
+                "Windows.Foundation.UniversalApiContract",
+                14);
+
+            return _isWin11.Value;
+        }
+
+        /// <inheritdoc/>
+        public string LocalFolderPath()
+        {
+            return ApplicationData.Current.LocalFolder.Path;
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Tools/ISystemInfoProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace JeniusApps.Common.Tools
+{
+    /// <summary>
+    /// Series of methods that retrieve information about the system.
+    /// </summary>
+    public interface ISystemInfoProvider
+    {
+        /// <summary>
+        /// Retrieves the culture name
+        /// in en-US format.
+        /// </summary>
+        string GetCulture();
+
+        /// <summary>
+        /// Returns string representing the device family.
+        /// </summary>
+        string GetDeviceFamily();
+
+        /// <summary>
+        /// Returns true if the current
+        /// session is the first time this app
+        /// was run since being installed.
+        /// </summary>
+        bool IsFirstRun();
+
+        /// <summary>
+        /// Returns true if the app is currently in compact mode.
+        /// </summary>
+        bool IsCompact();
+
+        /// <summary>
+        /// Returns true if the system is capable of using
+        /// the built-in fluent system icons.
+        /// </summary>
+        bool IsWin11();
+
+        /// <summary>
+        /// Returns the date time when the app was first used.
+        /// </summary>
+        /// <returns>DateTime when the app was first used.</returns>
+        DateTime FirstUseDate();
+
+        /// <summary>
+        /// Returns the local folder path for application data.
+        /// </summary>
+        string LocalFolderPath();
+    }
+}
