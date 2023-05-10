275ad2e8bf5989cf5cad73b469d50532f664fb70
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-05-09 21:47:44 -0700

    Added dispatcher queue

---
+++ src/JeniusApps.Common.Uwp/Tools/DispatcherQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.System;
+
+namespace JeniusApps.Common.Tools.Uwp
+{
+    public class WindowsDispatcherQueue : IDispatcherQueue
+    {
+        private readonly DispatcherQueue _dispatcherQueue;
+
+        public WindowsDispatcherQueue()
+        {
+            _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        }
+
+        /// <inheritdoc/>
+        public void TryEnqueue(Action action)
+        {
+            _dispatcherQueue.TryEnqueue(() => action());
+        }
+    }
+}
