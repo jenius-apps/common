6dcf36e8ee06983abf7221a530569c4c50aec820
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-05-09 23:12:52 -0700

    Added clipboard

---
+++ src/JeniusApps.Common.Uwp/Tools/WindowsClipboard.cs
@@ -0,0 +1,28 @@
+﻿using Windows.ApplicationModel.DataTransfer;
+
+namespace JeniusApps.Common.Tools.Uwp
+{
+    internal class WindowsClipboard : IClipboard
+    {
+        /// <inheritdoc/>
+        public bool CopyToClipboard(string text)
+        {
+            try
+            {
+                DataPackage dataPackage = new()
+                {
+                    RequestedOperation = DataPackageOperation.Copy
+                };
+
+                dataPackage.SetText(text);
+                Clipboard.SetContent(dataPackage);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Tools/IClipboard.cs
@@ -0,0 +1,15 @@
+﻿namespace JeniusApps.Common.Tools
+{
+    /// <summary>
+    /// Interface for clipboard functionality.
+    /// </summary>
+    public interface IClipboard
+    {
+        /// <summary>
+        /// Copies the given text to the system clipboard.
+        /// </summary>
+        /// <param name="text">The text to copy.</param>
+        /// <returns>True if the copy was successful. False, otherwise.</returns>
+        bool CopyToClipboard(string text);
+    }
+}
