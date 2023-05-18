bbe4a4f745c69fa1349b7f74d17e848d49e7c158
Daniel <dpaulino@users.noreply.github.com>
2023-05-17 22:13:50 -0700

    Added asset reader

---
+++ src/JeniusApps.Common.Uwp/Tools/AssetReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+
+#nullable enable
+
+namespace JeniusApps.Common.Tools.Uwp
+{
+    public class AssetReader : IAssetsReader
+    {
+        /// <inheritdoc/>
+        public async Task<string> ReadFileAsync(string relativePath)
+        {
+            StorageFolder currentFolder = Package.Current.InstalledLocation;
+            var splits = relativePath.Split(Path.DirectorySeparatorChar, StringSplitOptions.RemoveEmptyEntries);
+            StorageFile? file = null;
+
+            for (int i = 0; i < splits.Length; i++)
+            {
+                if (splits.Length - 1 == i)
+                {
+                    file = await currentFolder.GetFileAsync(splits[i]);
+                    break;
+                }
+
+                currentFolder = await currentFolder.GetFolderAsync(splits[i]);
+            }
+
+            return await FileIO.ReadTextAsync(file);
+        }
+    }
+}
---
+++ src/JeniusApps.Common/Tools/IAssetsReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace JeniusApps.Common.Tools
+{
+    /// <summary>
+    /// Reads data from the installed package location.
+    /// </summary>
+    public interface IAssetsReader
+    {
+        /// <summary>
+        /// Reads content from file at given relative path.
+        /// </summary>
+        /// <param name="relativePath">
+        /// The case sensitive relative path of the file to read in the installed package location.
+        /// E.g. 'Assets/data.json'
+        /// </param>
+        /// <returns>The string content of the given file.</returns>
+        Task<string> ReadFileAsync(string relativePath);
+    }
+}
