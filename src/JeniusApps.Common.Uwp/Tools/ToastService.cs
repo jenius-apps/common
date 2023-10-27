c31f2cfa19582ca091d8d5aab2b1f865544bdf9f
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-10-26 20:28:33 -0700

    Added toast service

---
+++ src/JeniusApps.Common.Uwp/Tools/ToastService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using Windows.UI.Notifications;
+
+#nullable enable
+
+namespace JeniusApps.Common.Tools.Uwp;
+
+public class ToastService : IToastService
+{
+    private readonly Lazy<IToastButton> _dismissButton;
+
+    public ToastService()
+    {
+        _dismissButton = new Lazy<IToastButton>(() => new ToastButtonDismiss());
+    }
+
+    /// <inheritdoc/>
+    public void ClearScheduledToasts()
+    {
+        try
+        {
+            ToastNotifierCompat notifier = ToastNotificationManagerCompat.CreateToastNotifier();
+            var scheduled = notifier.GetScheduledToastNotifications();
+
+            if (scheduled != null)
+            {
+                foreach (var toRemove in scheduled)
+                {
+                    notifier.RemoveFromSchedule(toRemove);
+                }
+            }
+        }
+        catch
+        {
+            // Crash telemetry suggests that sometimes, the 
+            // "notification platform" is unavailable, leading to a crash somehere here.
+            // We added the try-catch to try to mitigate the crash.
+        }
+    }
+
+    /// <inheritdoc/>
+    public void SendToast(
+        string title,
+        string message,
+        bool dismissVisible = false,
+        DateTime? scheduledDateTime = null,
+        string launchArg = "",
+        string tag = "",
+        Uri? audioUri = null,
+        bool audioSilent = false)
+    {
+        if (scheduledDateTime is not null &&
+            scheduledDateTime <= DateTime.Now)
+        {
+            return;
+        }
+
+        var builder = new ToastContentBuilder()
+            .SetToastScenario(ToastScenario.Default)
+            .AddText(title)
+            .AddText(message)
+            .AddArgument(launchArg);
+
+        if (audioUri is not null)
+        {
+            builder.AddAudio(audioUri, silent: audioSilent);
+        }
+
+        if (dismissVisible)
+        {
+            builder.AddButton(_dismissButton.Value);
+        }
+
+        if (scheduledDateTime is DateTime scheduledTime)
+        {
+            builder.Schedule(scheduledTime, toast =>
+            {
+                toast.Tag = tag;
+                toast.ExpirationTime = new DateTimeOffset(scheduledTime).AddMinutes(1);
+            });
+        }
+        else
+        {
+            builder.Show(toast =>
+            {
+                toast.Tag = tag;
+                toast.ExpirationTime = DateTimeOffset.Now.AddMinutes(1);
+            });
+        }
+    }
+
+    /// <inheritdoc/>
+    public bool DoesToastExist(string toastTag)
+    {
+        var toasts = ToastNotificationManager.History.GetHistory();
+        foreach (var toast in toasts)
+        {
+            if (toast.Tag == toastTag)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/JeniusApps.Common/Tools/IToastService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace JeniusApps.Common.Tools;
+
+/// <summary>
+/// Service for sending toasts.
+/// </summary>
+public interface IToastService
+{
+    /// <summary>
+    /// Clears the scheduled toasts.
+    /// </summary>
+    void ClearScheduledToasts();
+
+    /// <summary>
+    /// Pops the toast based on the given time.
+    /// If no time is provided, the toast will pop immediately.
+    /// </summary>
+    /// <param name="title">Title of the toast.</param>
+    /// <param name="message">Message body of the toast.</param>
+    /// <param name="dismissVisible">Adds dismiss button if true.</param>
+    /// <param name="scheduledDateTime">The time when the notification will be triggered.</param>
+    /// <param name="launchArg">Arguments that will be added to the toast. This is passed onto the app foreground when the toast is clicked.</param>
+    /// <param name="tag">A unique ID assigned to the toast that can be used for searching the toast later.</param>
+    /// <param name="audioUri">URI for an audio file that will be used as the notification sound.</param>
+    /// <param name="audioSilent">If true, the notification will not make a sound.</param>
+    void SendToast(
+        string title,
+        string message,
+        bool dismissVisible = false,
+        DateTime? scheduledDateTime = null,
+        string launchArg = "",
+        string tag = "",
+        Uri? audioUri = null,
+        bool audioSilent = false);
+
+    /// <summary>
+    /// Determines if the toast exists already.
+    /// </summary>
+    /// <param name="toastTag">A unique ID assigned to the toast that can be used for searching the toast later.</param>
+    /// <returns>True if the toast already exists.</returns>
+    bool DoesToastExist(string toastTag);
+
+}
