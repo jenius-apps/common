97197aabc22779a52a471ceb555fd04ead7b89cb
Daniel Paulino <dpaulino@users.noreply.github.com>
2023-11-29 22:01:40 -0800

    Initial attempt to add fade in

--- src/JeniusApps.Common.Uwp/Tools/WindowsMediaPlayer.cs
+++ src/JeniusApps.Common.Uwp/Tools/WindowsMediaPlayer.cs
@@ -1,104 +1,166 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Timers;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 
-namespace JeniusApps.Common.Tools.Uwp
+#nullable enable
+
+namespace JeniusApps.Common.Tools.Uwp;
+
+public class WindowsMediaPlayer : IMediaPlayer
 {
-    public class WindowsMediaPlayer : IMediaPlayer
+    private readonly MediaPlayer _player;
+    private Timer? _fadeInTimer;
+    private double _fadeInSingleStep;
+    private double _fadeInTargetVolume;
+    private long _fadeInStart;
+    private long _fadeInEnd;
+    private long _startEndDiff;
+
+    public event EventHandler<TimeSpan>? PositionChanged;
+
+    public WindowsMediaPlayer(bool disableSystemControls = false)
     {
-        private readonly MediaPlayer _player;
-
-        public event EventHandler<TimeSpan> PositionChanged;
-
-        public WindowsMediaPlayer(bool disableSystemControls = false)
+        var player = new MediaPlayer();
+        if (disableSystemControls)
         {
-            var player = new MediaPlayer();
-            if (disableSystemControls)
-            {
-                player.CommandManager.IsEnabled = false;
-            }
-            _player = player;
-            _player.PlaybackSession.PositionChanged += OnPlaybackPositionChanged;
+            player.CommandManager.IsEnabled = false;
+        }
+        _player = player;
+        _player.PlaybackSession.PositionChanged += OnPlaybackPositionChanged;
+    }
+
+    /// <inheritdoc/>
+    public TimeSpan Duration => _player.PlaybackSession.NaturalDuration;
+
+    /// <inheritdoc/>
+    public double Volume
+    {
+        get => _player.Volume;
+        set => _player.Volume = value;
+    }
+
+    /// <inheritdoc/>
+    public void Play(double? fadeInTargetVolume = null, double fadeInDuration = 0)
+    {
+        if (fadeInTargetVolume is null || fadeInTargetVolume <= 0 || fadeInDuration <= 0)
+        {
+            _player.Play();
+            return;
         }
 
-        /// <inheritdoc/>
-        public TimeSpan Duration => _player.PlaybackSession.NaturalDuration;
+        _fadeInTimer = new Timer();
+        var now = DateTime.Now;
+        _fadeInStart = now.Ticks;
+        _fadeInEnd = now.AddMilliseconds(fadeInDuration).Ticks;
+        _startEndDiff = _fadeInEnd - _fadeInStart;
+        _fadeInTimer.Elapsed += OnFadeInTimerTick;
+        _fadeInSingleStep = fadeInTargetVolume.Value / fadeInDuration;
+        _fadeInTargetVolume = fadeInTargetVolume.Value;
+        _player.Volume = 0;
+        _player.Play();
+        _fadeInTimer.Start();
+    }
 
-        /// <inheritdoc/>
-        public double Volume
+    private void OnFadeInTimerTick(object sender, ElapsedEventArgs e)
+    {
+        var currentTicks = e.SignalTime.Ticks - _fadeInStart;
+        double percent = (double)currentTicks / _startEndDiff;
+        Debug.WriteLine($"#################### {currentTicks} / {_startEndDiff} = {percent}");
+
+        if (percent >= 1)
         {
-            get => _player.Volume;
-            set => _player.Volume = value;
-        }
-
-        /// <inheritdoc/>
-        public void Play() => _player.Play();
-
-        /// <inheritdoc/>
-        public void Pause() => _player.Pause();
-
-        /// <inheritdoc/>
-        public void Dispose() => _player.Dispose();
-
-        /// <inheritdoc/>
-        public bool SetUriSource(Uri uriSource, bool enableGaplessLoop = false)
-        {
-            try
+            if (_fadeInTimer is { } timer)
             {
-                var mediaSource = MediaSource.CreateFromUri(uriSource);
-                AssignSource(mediaSource, enableGaplessLoop);
-            }
-            catch
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <inheritdoc/>
-        public async Task<bool> SetSourceAsync(string pathToFile, bool enableGaplessLoop = false)
-        {
-            try
-            {
-                StorageFile file = await StorageFile.GetFileFromPathAsync(pathToFile);
-                var mediaSource = MediaSource.CreateFromStorageFile(file);
-                AssignSource(mediaSource, enableGaplessLoop);
-            }
-            catch
-            {
-                return false;
+                timer.Elapsed -= OnFadeInTimerTick;
+                timer.Stop();
             }
 
-            return true;
+            _player.Volume = _fadeInTargetVolume;
+            _fadeInTimer = null;
+            Debug.WriteLine($"#################### stopped");
         }
-
-        private void AssignSource(MediaSource source, bool enableGaplessLoop)
+        else
         {
-            _player.Source = enableGaplessLoop
-                ? LoopEnabledPlaybackList(source)
-                : source;
+            _player.Volume = _fadeInTargetVolume * percent;
+            Debug.WriteLine($"#################### volume {_player.Volume}");
         }
+    }
 
-        private MediaPlaybackList LoopEnabledPlaybackList(MediaSource source)
+    /// <inheritdoc/>
+    public void Pause(double fadeOutDuration = 0)
+    {
+        if (fadeOutDuration <= 0)
         {
-            // This code here (combined with a wav source file) allows for gapless playback!
-            var item = new MediaPlaybackItem(source);
-            var playbackList = new MediaPlaybackList() { AutoRepeatEnabled = true };
-            playbackList.Items.Add(item);
-            return playbackList;
+            _player.Pause();
+            return;
         }
+    }
 
-        private void OnPlaybackPositionChanged(MediaPlaybackSession sender, object args)
+    /// <inheritdoc/>
+    public void Dispose() => _player.Dispose();
+
+    /// <inheritdoc/>
+    public bool SetUriSource(Uri uriSource, bool enableGaplessLoop = false)
+    {
+        try
         {
-            if (sender is null)
-            {
-                return;
-            }
-
-            PositionChanged?.Invoke(sender, sender.Position);
+            var mediaSource = MediaSource.CreateFromUri(uriSource);
+            AssignSource(mediaSource, enableGaplessLoop);
         }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public async Task<bool> SetSourceAsync(string pathToFile, bool enableGaplessLoop = false)
+    {
+        try
+        {
+            StorageFile file = await StorageFile.GetFileFromPathAsync(pathToFile);
+            var mediaSource = MediaSource.CreateFromStorageFile(file);
+            AssignSource(mediaSource, enableGaplessLoop);
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void AssignSource(MediaSource source, bool enableGaplessLoop)
+    {
+        _player.Source = enableGaplessLoop
+            ? LoopEnabledPlaybackList(source)
+            : source;
+    }
+
+    private MediaPlaybackList LoopEnabledPlaybackList(MediaSource source)
+    {
+        // This code here (combined with a wav source file) allows for gapless playback!
+        var item = new MediaPlaybackItem(source);
+        var playbackList = new MediaPlaybackList() { AutoRepeatEnabled = true };
+        playbackList.Items.Add(item);
+        return playbackList;
+    }
+
+    private void OnPlaybackPositionChanged(MediaPlaybackSession sender, object args)
+    {
+        if (sender is null)
+        {
+            return;
+        }
+
+        PositionChanged?.Invoke(sender, sender.Position);
     }
 }
--- src/JeniusApps.Common/Tools/IMediaPlayer.cs
+++ src/JeniusApps.Common/Tools/IMediaPlayer.cs
@@ -1,57 +1,56 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-namespace JeniusApps.Common.Tools
+namespace JeniusApps.Common.Tools;
+
+/// <summary>
+/// An abstraction over the system media player.
+/// </summary>
+public interface IMediaPlayer
 {
     /// <summary>
-    /// An abstraction over the system media player.
+    /// Raised when the position of the active playback item has changed.
     /// </summary>
-    public interface IMediaPlayer
-    {
-        /// <summary>
-        /// Raised when the position of the active playback item has changed.
-        /// </summary>
-        event EventHandler<TimeSpan> PositionChanged;
+    event EventHandler<TimeSpan> PositionChanged;
 
-        /// <summary>
-        /// Media player's volume.
-        /// </summary>
-        double Volume { get; set; }
+    /// <summary>
+    /// Media player's volume.
+    /// </summary>
+    double Volume { get; set; }
 
-        /// <summary>
-        /// Duration of the current playback item.
-        /// </summary>
-        TimeSpan Duration { get; }
-        
-        /// <summary>
-        /// Pauses the media player.
-        /// </summary>
-        void Pause();
+    /// <summary>
+    /// Duration of the current playback item.
+    /// </summary>
+    TimeSpan Duration { get; }
+    
+    /// <summary>
+    /// Pauses the media player.
+    /// </summary>
+    void Pause(double fadeOutDuration = 0);
 
-        /// <summary>
-        /// Plays the media player.
-        /// </summary>
-        void Play();
+    /// <summary>
+    /// Plays the media player.
+    /// </summary>
+    void Play(double? fadeInTargetVolume = null, double fadeInDuration = 0);
 
-        /// <summary>
-        /// Sets the media player's source using the given file path.
-        /// </summary>
-        /// <param name="pathToFile">Path of the source file.</param>
-        /// <param name="enableGaplessLoop">If true, the media source will be configured for gapless playback loop.</param>
-        /// <returns>True if setting the source was successful. False, otherwise.</returns>
-        Task<bool> SetSourceAsync(string pathToFile, bool enableGaplessLoop = false);
+    /// <summary>
+    /// Sets the media player's source using the given file path.
+    /// </summary>
+    /// <param name="pathToFile">Path of the source file.</param>
+    /// <param name="enableGaplessLoop">If true, the media source will be configured for gapless playback loop.</param>
+    /// <returns>True if setting the source was successful. False, otherwise.</returns>
+    Task<bool> SetSourceAsync(string pathToFile, bool enableGaplessLoop = false);
 
-        /// <summary>
-        /// Sets the media player's source using the given URI.
-        /// </summary>
-        /// <param name="uriSource">The URI to load into the media player.</param>
-        /// <param name="enableGaplessLoop">If true, the media source will be configured for gapless playback loop.</param>
-        /// <returns>True if setting the source was successful. False, otherwise.</returns>
-        bool SetUriSource(Uri uriSource, bool enableGaplessLoop = false);
+    /// <summary>
+    /// Sets the media player's source using the given URI.
+    /// </summary>
+    /// <param name="uriSource">The URI to load into the media player.</param>
+    /// <param name="enableGaplessLoop">If true, the media source will be configured for gapless playback loop.</param>
+    /// <returns>True if setting the source was successful. False, otherwise.</returns>
+    bool SetUriSource(Uri uriSource, bool enableGaplessLoop = false);
 
-        /// <summary>
-        /// Releases resources associated with the media player.
-        /// </summary>
-        void Dispose();
-    }
+    /// <summary>
+    /// Releases resources associated with the media player.
+    /// </summary>
+    void Dispose();
 }
