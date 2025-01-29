098df412ce551d0d85f76615aecf8e86152b621b
Daniel Paulino <dpaulino@users.noreply.github.com>
2025-01-28 23:40:00 -0800

    Added file writer

---
+++ src/JeniusApps.Common.Uwp/Tools/LocalFileWriter.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+#nullable enable
+
+namespace JeniusApps.Common.Tools.Uwp;
+
+public class LocalFileWriter : ILocalFileWriter
+{
+    private readonly ConcurrentDictionary<string, SemaphoreSlim> _semaphores = new();
+
+    /// <inheritdoc/>
+    public async Task<bool> DeleteAsync(string absolutePathInLocalStorage, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+
+        if (string.IsNullOrEmpty(absolutePathInLocalStorage))
+        {
+            return false;
+        }
+
+        try
+        {
+            var file = await StorageFile.GetFileFromPathAsync(absolutePathInLocalStorage);
+            token.ThrowIfCancellationRequested();
+            await file.DeleteAsync();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    /// <inheritdoc/>
+    public async Task<string> ReadAsync(string relativeLocalPath, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+
+        if (string.IsNullOrEmpty(relativeLocalPath))
+        {
+            return string.Empty;
+        }
+
+        var semaphore = _semaphores.GetOrAdd(relativeLocalPath, new SemaphoreSlim(1, 1));
+        await semaphore.WaitAsync();
+
+        try
+        {
+            IStorageItem targetLocation = await ApplicationData.Current.LocalFolder.TryGetItemAsync(relativeLocalPath);
+            token.ThrowIfCancellationRequested();
+
+            if (targetLocation is StorageFile file)
+            {
+                return await FileIO.ReadTextAsync(file);
+            }
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+
+        return string.Empty;
+    }
+
+    /// <inheritdoc/>
+    public async Task<string?> WriteAsync(Stream stream, string nameWithExt, CancellationToken token, string? localDirName = null)
+    {
+        token.ThrowIfCancellationRequested();
+
+        string relativeLocalPath = localDirName is null
+            ? nameWithExt
+            : $"{localDirName}/{nameWithExt}";
+
+        if (string.IsNullOrEmpty(relativeLocalPath))
+        {
+            return null;
+        }
+
+        var semaphore = _semaphores.GetOrAdd(relativeLocalPath, new SemaphoreSlim(1, 1));
+        await semaphore.WaitAsync();
+        try
+        {
+            StorageFile targetLocation = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                relativeLocalPath,
+                CreationCollisionOption.ReplaceExisting);
+
+            token.ThrowIfCancellationRequested();
+
+            if (targetLocation is not null)
+            {
+                using IRandomAccessStream fileStream = await targetLocation.OpenAsync(FileAccessMode.ReadWrite);
+                await stream.CopyToAsync(fileStream.AsStreamForWrite());
+                await fileStream.FlushAsync();
+
+                return targetLocation.Path;
+            }
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+
+        return null;
+    }
+
+    /// <inheritdoc/>
+    public async Task<string?> WriteAsync(string content, string nameWithExt, CancellationToken token, string? localDirName = null)
+    {
+        token.ThrowIfCancellationRequested();
+
+        string relativeLocalPath = localDirName is null
+            ? nameWithExt
+            : $"{localDirName}/{nameWithExt}";
+
+        if (string.IsNullOrEmpty(relativeLocalPath))
+        {
+            return null;
+        }
+
+        var semaphore = _semaphores.GetOrAdd(relativeLocalPath, new SemaphoreSlim(1, 1));
+        await semaphore.WaitAsync();
+        try
+        {
+            StorageFile targetLocation = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                relativeLocalPath,
+                CreationCollisionOption.ReplaceExisting);
+
+            token.ThrowIfCancellationRequested();
+
+            if (targetLocation is not null)
+            {
+                await FileIO.WriteTextAsync(targetLocation, content);
+                return targetLocation.Path;
+            }
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+
+        return null;
+    }
+}
---
+++ src/JeniusApps.Common/Tools/ILocalFileWriter.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JeniusApps.Common.Tools;
+
+/// <summary>
+/// Interface for reading and writing files to the local application folder.
+/// </summary>
+public interface ILocalFileWriter
+{
+    /// <summary>
+    /// Generic method for writing a file to local directory.
+    /// </summary>
+    /// <param name="stream">The stream of the file.</param>
+    /// <param name="nameWithExt">The name of the file with extension. E.g. Wind.jpg.</param>
+    /// <param name="localDirName">Optional. The name of the subdirectory to place the file in. 
+    /// <param name="token">Cancellation token.</param>
+    /// If null or empty, then the root local directory will be used.</param>
+    /// <returns>Full path of the written file.</returns>
+    Task<string?> WriteAsync(Stream stream, string nameWithExt, CancellationToken token, string? localDirName = null);
+
+    /// <summary>
+    /// Generic method for writing string content to a file in the local directory.
+    /// </summary>
+    /// <param name="content">The string content to write.</param>
+    /// <param name="nameWithExt">The name of the file with extension. E.g. Wind.jpg.</param>
+    /// <param name="localDirName">Optional. The name of the subdirectory to place the file in. 
+    /// <param name="token">Cancellation token.</param>
+    /// If null or empty, then the root local directory will be used.</param>
+    /// <returns>Full path of the written file.</returns>
+    Task<string?> WriteAsync(string content, string nameWithExt, CancellationToken token, string? localDirName = null);
+
+    /// <summary>
+    /// Reads the contents of the specified local file and returns the value.
+    /// Returns string.Empty if file not found or path is invalid.
+    /// </summary>
+    /// <param name="relativeLocalPath">The relative path for a local file to read.</param>
+    /// <param name="token">Cancellation token.</param>
+    /// <returns>The string content of the file.</returns>
+    Task<string> ReadAsync(string relativeLocalPath, CancellationToken token);
+
+    /// <summary>
+    /// Deletes the given file.
+    /// </summary>
+    /// <param name="absolutePathInLocalStorage">The absolute path for a file inside local storage.
+    /// If the path is outside of local storage, deletion will likely fail due to file permission restrictions.</param>
+    /// <param name="token">Cancellation token.</param>
+    /// <returns>True if successful, false otherwise.</returns>
+    Task<bool> DeleteAsync(string absolutePathInLocalStorage, CancellationToken token);
+}
