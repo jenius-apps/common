7759fb6054f3c550f1b231c6b75e5d4c69b5602e
Daniel <dpaulino@users.noreply.github.com>
2025-06-16 21:04:41 -0700

    Adding push notification helpers (#3)
    * Added push notification logic
    
    * Updated to 0.0.47-preview

---
+++ src/JeniusApps.Common/PushNotifications/IPushNotificationSource.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace JeniusApps.Common.PushNotifications;
+
+/// <summary>
+/// Interface for retrieving push data from a Push Notification platform.
+/// </summary>
+public interface IPushNotificationSource
+{
+    /// <summary>
+    /// Registers for push notifications with the source platform
+    /// and retrieves the notification URI.
+    /// </summary>
+    /// <param name="ct">A cancellation token.</param>
+    /// <returns>If successful, returns a URI to be used for the push notification. Otherwise returns null.</returns>
+    Task<string?> GetNotificationUriAsync(CancellationToken ct);
+}
---
+++ src/JeniusApps.Common/PushNotifications/PushNotificationService.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JeniusApps.Common.PushNotifications;
+
+/// <summary>
+/// Main orchestrator for registering push notifications.
+/// </summary>
+public class PushNotificationService : IPushNotificationService
+{
+    private readonly IPushNotificationSource _source;
+    private readonly IPushNotificationStorage _storage;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="source">The source that provides the push notification URL for each registered device.</param>
+    /// <param name="storage">The storage where the registrations are saved.</param>
+    public PushNotificationService(
+        IPushNotificationSource source,
+        IPushNotificationStorage storage)
+    {
+        _source = source;
+        _storage = storage;
+    }
+
+    /// <inheritdoc/>
+    public async Task<bool> RegisterAsync(string deviceId, string primaryLanguageCode, CancellationToken ct)
+    {
+        ct.ThrowIfCancellationRequested();
+
+        if (string.IsNullOrEmpty(deviceId))
+        {
+            ThrowHelper.ThrowArgumentException(nameof(deviceId));
+        }
+
+        if (string.IsNullOrEmpty(primaryLanguageCode))
+        {
+            ThrowHelper.ThrowArgumentException(nameof(primaryLanguageCode));
+        }
+
+        string? uri = await _source.GetNotificationUriAsync(ct);
+        if (uri is not { Length: > 0 })
+        {
+            return false;
+        }
+
+        DeviceRegistrationData data = new()
+        {
+            ActionRequested = "register",
+            DeviceId = deviceId,
+            PrimaryLanguageCode = primaryLanguageCode,
+            Uri = uri
+        };
+
+        return await _storage.RegisterDeviceAsync(data, ct);
+    }
+
+    /// <inheritdoc/>
+    public async Task UnregisterAsync(string deviceId, CancellationToken ct)
+    {
+        ct.ThrowIfCancellationRequested();
+        await _storage.DeleteDeviceRegistrationAsync(deviceId, ct);
+    }
+}
