48b8bbeb95a75209837e22bb9701bac05422512d
Daniel Paulino <dpaulino@users.noreply.github.com>
2025-06-16 21:02:55 -0700

    Added push notification logic

---
+++ src/JeniusApps.Common.Uwp/PushNotifications/WindowsPushNotificationSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking.PushNotifications;
+
+#nullable enable
+
+namespace JeniusApps.Common.PushNotifications.Uwp;
+
+/// <summary>
+/// An implementation of the notification source for the Windows Notification Service.
+/// </summary>
+public sealed class WindowsPushNotificationSource : IPushNotificationSource
+{
+    /// <inheritdoc/>
+    public async Task<string?> GetNotificationUriAsync(CancellationToken ct)
+    {
+        ct.ThrowIfCancellationRequested();
+
+        try
+        {
+            PushNotificationChannel? channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+            return channel?.Uri;
+        }
+        catch
+        {
+            return null;
+        }        
+    }
+}
